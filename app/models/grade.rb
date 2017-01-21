64324e313d575a70d2d67cbb9714ae229920ecbf
Libo Zhang <zhang1li2bo3@qq.com>
2017-01-21 04:35:59 +0000

    Removed grade validation from grade model

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,5 +1,5 @@
 class Grade < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
-  validates :grade, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
+  # validates :grade, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }
 end
